013ae266e512bc9fe3e365393ffa6f152fac7caf
muralimano <muralimanoharsoft@gmail.com>
2019-01-05 13:33:28 +0530

    Initial commit

---
+++ app/src/main/java/com/muralimanohar/paytm/Constants.java
@@ -0,0 +1,11 @@
+package com.muralimanohar.paytm;
+
+public class Constants {
+
+    public static final String M_ID = "rxazcv89315285244163"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WEB"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+    public static final String WEBSITE = "APP_STAGING";
+    public static final String CALLBACK_URL = "https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=order1";
+
+}
